549165aec11a5808bc6ddde65d0d076f75e931ab
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2023-04-27 14:33:58 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "demo-sg" {
-  name = "demo-sg"
+resource "aws_security_group" "jenkins-terraform-SG" {
+  name = "jenkins-terraform-SG"
   description = "allow ssh and http traffic"
   
   ingress {
