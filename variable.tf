4b2e64b9412c1a712ac7c9a2ad8e001e2e4f4213
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2023-04-27 14:48:17 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-   default = ""
+   default = "AKIAUIH6I7ZT6YX2TJ3G"
    }
 variable "secret_key" {
-   default = ""
+   default = "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"
    }
 variable "region" {
    default = "ap-south-1"
